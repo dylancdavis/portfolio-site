70ec822925c4cc1786c1612742d91c33bb541931
dylancdavis <dylcdav@gmail.com>
2023-04-04 11:49:38 -0400

    Add introduction paragraphs

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
     <div className="App">
       <header>
         <div>
-          <h1 class="title-text">
-            <span class="invertable">d</span>
-            <span class="invertable">y</span>
-            <span class="invertable">l</span>
-            <span class="invertable">c</span>
-            <span class="invertable">d</span>
-            <span class="invertable">a</span>
-            <span class="invertable">v</span>
+          <h1 className="title-text">
+            <span className="invertable">d</span>
+            <span className="invertable">y</span>
+            <span className="invertable">l</span>
+            <span className="invertable">c</span>
+            <span className="invertable">d</span>
+            <span className="invertable">a</span>
+            <span className="invertable">v</span>
           </h1>
         </div>
         <nav>
@@ -24,13 +24,31 @@ function App() {
       </header>
       <hr></hr>
       <main>
-        <div class="intro-box">
-          <h2 class="intro-text">
-            hello! i'm a
-            <div class="hero-text">
-              <span className="muted">partially</span> self-taught web developer
-            </div>
-          </h2>
+        <div className="intro-box">
+          <div className="intro-text">
+            <h2>
+              Hello! I'm Dylan Davis, a
+              <div className="hero-text">
+                <span className="muted">(partially)</span> self-taught
+                <div>web developer.</div>
+              </div>
+            </h2>
+            <p>
+              I started learning how to create websites nearly eight years ago,
+              and began studying computer science seriously at the{" "}
+              <span className="garnet">
+                University of South Carolina in 2018.
+              </span>{" "}
+              I continued my pursuit of software development after graduation,
+              focusing on fundamentals and building side projects.
+            </p>
+            <p>
+              This year, I enrolled with the University of Helsinki to learn
+              more about full-stack web development. I've build a few projects
+              with React, and I've primarily worked with the MERN stack, though
+              I've gotten to know Vue more recently as well.
+            </p>
+          </div>
         </div>
         <hr></hr>
         <section>
