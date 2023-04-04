cee8162ffe2ae7416531d9c93e29901cb572a957
dylancdavis <dylcdav@gmail.com>
2023-04-04 09:48:29 -0400

    Add title and subtext scaffolding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world!</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          <span class="raisable">d</span>
+          <span class="raisable">y</span>
+          <span class="raisable">l</span>
+          <span class="raisable">c</span>
+          <span class="raisable">d</span>
+          <span class="raisable">a</span>
+          <span class="raisable">v</span>
+        </h1>
+      </header>
+      <div class="hero-text">
+        <span className="muted">partially</span> self-taught web developer
+      </div>
+    </div>
+  );
 }
 
 export default App;
