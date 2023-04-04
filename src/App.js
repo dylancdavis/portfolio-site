2ef3e66d6e3d8665ebddddee49662d335cf536c5
dylancdavis <dylcdav@gmail.com>
2023-04-04 11:40:54 -0400

    Reorganize and style introduction section

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1 class="title-text">
-          <span class="invertable">d</span>
-          <span class="invertable">y</span>
-          <span class="invertable">l</span>
-          <span class="invertable">c</span>
-          <span class="invertable">d</span>
-          <span class="invertable">a</span>
-          <span class="invertable">v</span>
-        </h1>
+        <div>
+          <h1 class="title-text">
+            <span class="invertable">d</span>
+            <span class="invertable">y</span>
+            <span class="invertable">l</span>
+            <span class="invertable">c</span>
+            <span class="invertable">d</span>
+            <span class="invertable">a</span>
+            <span class="invertable">v</span>
+          </h1>
+        </div>
         <nav>
           <div>home</div>
           <div>projects</div>
@@ -22,12 +24,14 @@ function App() {
       </header>
       <hr></hr>
       <main>
-        <h2 class="intro-text">
-          hello! i'm a
-          <div class="hero-text">
-            <span className="muted">partially</span> self-taught web developer
-          </div>
-        </h2>
+        <div class="intro-box">
+          <h2 class="intro-text">
+            hello! i'm a
+            <div class="hero-text">
+              <span className="muted">partially</span> self-taught web developer
+            </div>
+          </h2>
+        </div>
         <hr></hr>
         <section>
           <h2>projects</h2>
