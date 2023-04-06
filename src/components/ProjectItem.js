8766a2c8bb7374912e16753c3055fb8ac5c28960
dylancdavis <dylcdav@gmail.com>
2023-04-06 15:39:27 -0400

    Add ProjectItem and lay groundwork for ProjectsPage

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,16 @@
+const ProjectItem = ({ name, image, website, github, description }) => {
+  return (
+    <div
+      class="project-item"
+      style={{ border: "1px solid black", padding: "8px" }}
+    >
+      <h4>{name}</h4>
+      <div class="project-image">{image}</div>
+      {website && <a href={website}>Link</a>}
+      <a href={github}>Github</a>
+      {description && <p>{description}</p>}
+    </div>
+  );
+};
+
+export default ProjectItem;
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,10 +1,40 @@
+import ProjectItem from "./ProjectItem";
+
 const ProjectsPage = () => {
   return (
     <>
-      <h2>Projects</h2>
-      <ul>
-        <li>Blogsharing Application</li>
-      </ul>
+      <section>
+        <h3>Web Development</h3>
+        <div class="project-container">
+          <ProjectItem
+            name="Blogsharing Application"
+            image="image placeholder"
+            website="http://blog-sharing.fly.dev/"
+            github="https://github.com/dylancdavis/blog-sharing"
+            description="This is a blog sharing application that allows users to create and share blogs with other users. Users can also comment on blogs and like blogs. This application was built using React, Node.js, Express, and MongoDB."
+          />
+          <ProjectItem
+            name="To-do list Application"
+            image="image placeholder"
+            github="https://github.com/dylancdavis/todo-app"
+            description="My todo list application"
+          />
+          <ProjectItem
+            name="SCOTA Mentorship Quiz"
+            image="image placeholder"
+            website="http://scotamentorshipquiz.info/"
+            github="https://github.com/dylancdavis/quiz-site"
+            description="A quiz site created for use by the SC Occupational Therapy Association's mentorship program."
+          />
+          <ProjectItem
+            name="Online Calculator"
+            image="image placeholder"
+            website="https://dylancdavis.github.io/online-calculator/"
+            github="https://github.com/dylancdavis/online-calculator"
+            description="An online calculator created using vanilla HTML, CSS, and JavaScript."
+          />
+        </div>
+      </section>
     </>
   );
 };
